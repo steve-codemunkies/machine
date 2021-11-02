99576c32646c76ac730d86b291a4e96661d2cd2c
steve-codemunkies <steve@codemunki.es>
2021-11-02 08:35:28 +0000

    Add packages to remove

--- windows/Setup/Prereqs.ps1
+++ windows/Setup/Prereqs.ps1
@@ -78,8 +78,11 @@ Get-AppxPackage Microsoft.SkypeApp | Remove-AppxPackage
 Get-AppxPackage Microsoft.Microsoft3DViewer | Remove-AppxPackage
 Get-AppxPackage Microsoft.MixedReality.Portal | Remove-AppxPackage
 Get-AppxPackage Microsoft.ZuneMusic | Remove-AppxPackage
+Get-AppxPackage Microsoft.ZuneVideo | Remove-AppxPackage
 Get-AppxPackage Microsoft.YourPhone | Remove-AppxPackage
 Get-AppxPackage Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage
+Get-AppxPackage MicrosoftTeams | Remove-AppxPackage
+
 
 ##########################################################################
 # Privacy
